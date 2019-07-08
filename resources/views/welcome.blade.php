0f6df7f4c5a16dc9c1334451da1ebcf4ae262089
iameronmosele <crayolu@gmail.com>
2019-07-08 09:42:31 +0100

    making changes and update to the api

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -66,6 +66,23 @@
 </head>
 <body>
 <div class="container">
+    <!-- A grey horizontal navbar that becomes vertical on small screens -->
+    <nav class="navbar navbar-expand-sm bg-light">
+
+        <!-- Links -->
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link 1</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link 2</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link 3</a>
+            </li>
+        </ul>
+
+    </nav>
     <div class="row">
         <div class="col-lg-12">
             <h1 id="show-episodes">Show Episodes</h1>
